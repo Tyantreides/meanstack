85991574eef9feb4e9daeb919f47c84a78f83f84
Christian Meissner <c.meissner@k-newmedia.de>
2015-10-13 15:34:23 +0200

    user crm init

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-	'port': process.env.PORT || 1338,
-	'database': 'mongodb://localhost:27017/meanappstructure',
+	'port': process.env.PORT || 8080,
+	'database': 'mongodb://node:noder@novus.modulusmongo.net:27017/Iganiq8o',
 	'secret': 'ilovescotchscotchyscotchscotch'
 };
